429b9524b9dd2ba5dd98ea2dfb38eac51668c828
simplepro <simplepro1254@gmail.com>
2020-07-19 14:16:37 +0900

    cleanSearch 첫 커밋

---
+++ app/src/main/java/com/example/cleansearch/activity/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.cleansearch.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.GridLayout
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cleansearch.R
+import com.example.cleansearch.adapter.FieldWordRecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var spinnerList : ArrayList<String> = arrayListOf("선택", "개발", "과학", "역사", "수학", "공부", "진로", "건강", "운동")
+    var spinnerAdapter : ArrayAdapter<String>? = null
+    var selectField : String = "선택"
+    var fieldWordList : ArrayList<String> = arrayListOf()
+    var fieldWordAdapter : FieldWordRecyclerView = FieldWordRecyclerView(fieldWordList)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerViewLayoutManager = GridLayoutManager(applicationContext, 2)
+
+        val fieldWordMap = mapOf<String, ArrayList<String>>("선택" to arrayListOf(),
+        "개발" to arrayListOf("stackOverFlow", "StackOverFlow", "개발자", "개발"), "과학" to arrayListOf("돌", "화산", "지진", "화석", "화성", "우주", "지구", "땅", "물", "불", "흙", "공기"),
+        "역사" to arrayListOf("지리", "역사"), "수학" to arrayListOf("더하기", "나누기", "빼기", "곱하기"), "공부" to arrayListOf("과목", "학교"), "진로" to arrayListOf("꿈", "대학교", "취직", "취업"),
+        "건강" to arrayListOf("건강", "양파"), "운동" to arrayListOf("축구", "농구", "야구", "배구", "배드민턴", "탁구"))
+
+        spinnerAdapter = ArrayAdapter(this,
+            R.layout.item_field_spinner, spinnerList)
+        fieldSpinner.adapter = spinnerAdapter
+
+        fieldSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                selectField = spinnerList[position]
+                Log.d("TAG", "selectField is $selectField")
+                fieldWordList = fieldWordMap[selectField]!!
+                Log.d("TAG", "fieldWordList is $fieldWordList")
+                fieldWordAdapter = FieldWordRecyclerView(fieldWordList)
+                fieldWordAdapter.notifyDataSetChanged()
+                fieldWordRecyclerView.adapter = fieldWordAdapter
+            }
+        }
+        fieldWordRecyclerView.apply {
+            adapter = fieldWordAdapter
+//            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+            layoutManager = recyclerViewLayoutManager
+            setHasFixedSize(true)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cleansearch/adapter/FieldWordRecyclerView.kt
@@ -0,0 +1,33 @@
+package com.example.cleansearch.adapter
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cleansearch.R
+
+class FieldWordRecyclerView(val FieldList : ArrayList<String>) : RecyclerView.Adapter<FieldWordRecyclerView.CustomViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): FieldWordRecyclerView.CustomViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_field_recycler_view, parent, false)
+        return CustomViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        Log.d("TAG", "getItemCount")
+        return FieldList.size
+    }
+
+    override fun onBindViewHolder(holder: FieldWordRecyclerView.CustomViewHolder, position: Int) {
+        holder.fieldWordText.text = FieldList[position]
+        Log.d("TAG", "FiledList[$position] is ${FieldList[position]}")
+    }
+
+    class CustomViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val fieldWordText = itemView.findViewById<TextView>(R.id.fieldWordTextView)
+    }
+}
