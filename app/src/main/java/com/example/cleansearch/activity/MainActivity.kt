7e4e97b5402ff2b82c064cd85a6447179e20ca45
simplepro <simplepro1254@gmail.com>
2020-07-20 14:55:22 +0900

    window 조절.

--- app/src/main/java/com/example/cleansearch/activity/MainActivity.kt
+++ app/src/main/java/com/example/cleansearch/activity/MainActivity.kt
@@ -129,6 +129,12 @@ class MainActivity : AppCompatActivity(), KeyWordRecyclerViewAdapter.ItemViewSet
         }
         //addKeyWordButton
         addKeyWordButton.setOnClickListener {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             if(KeyWordEditText.text.isNotEmpty())
             {
                 keyWordList.add(KeyWordEditText.text.toString())
@@ -196,6 +202,12 @@ class MainActivity : AppCompatActivity(), KeyWordRecyclerViewAdapter.ItemViewSet
         KeyWordBuilder.show()
 
         KeyWordRemoveButtonDialog.setOnClickListener {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             keyWordList.removeAt(position)
             if(keyWordList.size == 0)
             {
@@ -208,6 +220,12 @@ class MainActivity : AppCompatActivity(), KeyWordRecyclerViewAdapter.ItemViewSet
         }
 
         KeyWordCancelButtonDialog.setOnClickListener {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             KeyWordBuilder.dismiss()
         }
 
