a79e656122c457bcd5c4a72f2b15b3d0f750543b
simplepro <simplepro1254@gmail.com>
2020-08-27 17:59:18 +0900

    고정 IP 로 수정.

--- app/src/main/java/com/wotin/cleansearch/Activity/AddAndReplaceSynonymActivity.kt
+++ app/src/main/java/com/wotin/cleansearch/Activity/AddAndReplaceSynonymActivity.kt
@@ -39,7 +39,7 @@ class AddAndReplaceSynonymActivity : AppCompatActivity(), AddAndReplaceSynonymRe
     var synonymArrayList : ArrayList<String> = arrayListOf()
     lateinit var synonymAdapter : AddAndReplaceSynonymRecyclerViewAdapter
 
-    val baseUrl = "http://172.25.16.1:8080"
+    val baseUrl = "http://192.168.0.254:8080"
     lateinit var retrofit : Retrofit
     lateinit var apiService : RetrofitClean
 
