6c16ac69632817fef2b75889ad48fb1314d9ffe4
simplepro <simplepro1254@gmail.com>
2020-10-09 18:01:19 +0900

    baseUrl 바꿈.

--- app/src/main/java/com/wotin/cleansearch/Activity/AddAndReplaceSynonymActivity.kt
+++ app/src/main/java/com/wotin/cleansearch/Activity/AddAndReplaceSynonymActivity.kt
@@ -39,7 +39,7 @@ class AddAndReplaceSynonymActivity : AppCompatActivity(), AddAndReplaceSynonymRe
     var synonymArrayList : ArrayList<String> = arrayListOf()
     lateinit var synonymAdapter : AddAndReplaceSynonymRecyclerViewAdapter
 
-    val baseUrl = "http://220.72.174.101:8080"
+    val baseUrl = "http://220.117.41.156:8080"
     lateinit var retrofit : Retrofit
     lateinit var apiService : RetrofitClean
 
